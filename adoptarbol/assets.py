c20b94007aec668c80556207434893a156fa9d1e
Sebastian Silva <sebastian@fuentelibre.org>
2018-01-05 02:16:16 -0500

    Big bootstrap removal

--- adoptarbol/assets.py
+++ adoptarbol/assets.py
@@ -4,16 +4,16 @@ from flask_assets import Bundle, Environment
 
 css = Bundle(
     # 'libs/bootstrap/dist/css/bootstrap.css',
-    'css/bootstrap.css',
+    # 'css/bootstrap.css',
     'css/style.css',
     filters='cssmin',
     output='public/css/common.css'
 )
 
 js = Bundle(
-    'libs/jQuery/dist/jquery.js',
-    'libs/bootstrap/dist/js/bootstrap.js',
-    'js/plugins.js',
+    # 'libs/jQuery/dist/jquery.js',
+    # 'libs/bootstrap/dist/js/bootstrap.js',
+    # 'js/plugins.js',
     filters='jsmin',
     output='public/js/common.js'
 )
